1eca20b2cb887989cc392211df194e68c37be1af
Pepi Dimi <pepi55@live.com>
2013-12-04 16:40:53 +0100

    divisor & divident

--- python/pythonTests.py
+++ python/pythonTests.py
@@ -6,5 +6,11 @@ from Fraction import *
 a = Fraction(1, 2)
 b = Fraction(2, 5)
 c = multiplyFraction(a, b)
+d = divideFraction(a, b)
+e = substractFraction(a, b)
+f = addFraction(a, b)
 
 print(c.reduced_numerator, c.reduced_denominator)
+print(d.reduced_numerator, d.reduced_denominator)
+print(e.reduced_numerator, e.reduced_denominator)
+print(f.reduced_numerator, f.reduced_denominator)
