3916534c608e3be91fda90fb1825a9a675dc6a97
Pepi Dimi <pepi55@live.com>
2013-12-05 11:00:02 +0100

    python update

--- python/pythonTests.py
+++ python/pythonTests.py
@@ -2,15 +2,20 @@
 
 from math import *
 from Fraction import *
+from Ratio import *
 
-a = Fraction(1, 2)
-b = Fraction(2, 5)
+a = Fraction(1024, 768)
+b = Fraction(1280, 1024)
 c = multiplyFraction(a, b)
 d = divideFraction(a, b)
 e = substractFraction(a, b)
 f = addFraction(a, b)
 
-print(c.reduced_numerator, c.reduced_denominator)
-print(d.reduced_numerator, d.reduced_denominator)
-print(e.reduced_numerator, e.reduced_denominator)
-print(f.reduced_numerator, f.reduced_denominator)
+ratio = Ratio(f.numerator, f.denominator)
+scaled = scaleDivisor(ratio, 500)
+
+#print(c.reduced_numerator, c.reduced_denominator)
+#print(d.reduced_numerator, d.reduced_denominator)
+#print(e.reduced_numerator, e.reduced_denominator)
+#print(f.reduced_numerator, f.reduced_denominator)
+print(scaled.divident, scaled.divisor)
