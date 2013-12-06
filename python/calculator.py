71cce085b5598ef1764ebeb17e728516d9b76db3
Pepi Dimi <pepi55@live.com>
2013-12-06 08:43:00 +0100

    added calculator

---
+++ python/calculator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from math import *
+from Fraction import *
+from Ratio import *
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+mainScreen = ttk.Frame(root, padding = "10 10 10 10")
+
+root.title("Ratio scaling calculator")
