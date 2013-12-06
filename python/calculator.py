43c4d7cdd0af584c95e62237c024f6ede1e38a4c
Pepi Dimi <pepi55@live.com>
2013-12-06 10:30:27 +0100

    updated calculator

--- python/calculator.py
+++ python/calculator.py
@@ -3,10 +3,39 @@
 from math import *
 from Fraction import *
 from Ratio import *
-from tkinter import *
-from tkinter import ttk
+from tkinter.ttk import *
+
+import tkinter as tk
+
+class Calculator(tk.Frame):
+	def __init__(self, master = None):
+		tk.Frame.__init__(self, master)
+		self.pack()
+		self.createWidgets()
+
+	def createWidgets(self):
+		self.EXIT = tk.Button(self, text = "QUIT", fg = "red", command = root.destroy)
+		self.EXIT.pack(side = "bottom")
 
 root = Tk()
-mainScreen = ttk.Frame(root, padding = "10 10 10 10")
+calc = Calculator(master = root)
 
-root.title("Ratio scaling calculator")
+mainframe = ttk.Frame(root, padding = "3 3 12 12")
+
+ratio1 = StringVar()
+ratioAnsw = StringVar()
+
+ratioEntry = ttk.Entry(mainframe, width = 7, textVar = ratio1)
+
+root.title("Ratio convertion")
+
+mainframe.grid(column = 0, row = 0, sticky = (N, W, E, S))
+mainframe.columnconfigure(0, weight = 1)
+mainframe.rowconfigure(0, weight = 1)
+
+ratioEntry.grid(column = 2, row = 1, sticky = (W, E))
+
+ttk.Label(mainframe, textvariable = ratioAnsw).grid(column = 2, row = 2, sticky = (W, E))
+ttk.Button(mainframe, text = "Calculate", command = calculate).grid(column = 3, row = 3, sticky = W)
+
+calc.mainloop()
