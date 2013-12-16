ce6f9e0f9f55cfbc70d95ea1803d295aab4a00a8
Pepi Dimi <pepi55@live.com>
2013-12-16 18:38:54 +0100

    frustrating C++ is frustrating

--- c++/math.cpp
+++ c++/math.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <math.h>
 
-int isInteger (int a) {
+using namespace std;
+
+float a;
+float f = 12.3;
+
+float isInteger (float a) {
 	bool result = false;
+
+	cout << a << "\n";
+
+	if (a % 1 == 0) {
+		result = true;
+	}
+
+	return result;
+}
+
+int main () {
+	cout << isInteger(f);
+	//cout << "\n" << f;
+
+	return 0;
 }
