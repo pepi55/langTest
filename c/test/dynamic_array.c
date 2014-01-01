42e901685877e6de19c6fac1e0de3a756cab0584
pepi <pepi55@live.com>
2014-01-01 02:59:19 +0100

    maths.c framwork finished

---
+++ c/test/dynamic_array.c
@@ -0,0 +1,6 @@
+#include "dynamic_array.h"
+
+long *dynamic_array (long n) {
+	int i, l = 1;
+	unsigned int *result;
+}
---
+++ c/test/dynamic_array.h
@@ -0,0 +1,6 @@
+#ifndef DYNAMIC_ARRAY_H_INCLUDED
+#define DYNAMIC_ARRAY_H_INCLUDED
+
+long *dynamic_array(long n);
+
+#endif
