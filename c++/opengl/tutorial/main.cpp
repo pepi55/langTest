7c85bc4847233ba00ea60b865202a87b72405f58
Pepi Dimi <pepi55@live.com>
2014-09-23 14:23:16 +0200

    C++ classes

--- c++/opengl/tutorial/main.cpp
+++ c++/opengl/tutorial/main.cpp
@@ -1,5 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//OpenGL includes
 #include <GL/glew.h>
-#include <GL/glfw3.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+//User includes
+#include "src/openglFunc.hpp"
+
+using namespace glm;
+
+int main (void) {
+	if (!glfwInit()) {
+		fprintf(stderr, "Failed to initialize GLFW!\n");
+		
+		return -1;
+	}
+
+	return 0;
+}
